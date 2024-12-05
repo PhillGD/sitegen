8039e27c43035eabfaf43071daca04f808f97ce6
PhillGD <phillgd@outlook.com>
2024-12-05 13:29:51 +0000

    add ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,7 +10,7 @@ class HTMLNode:
     
     def props_to_html(self):
         props_string = ""
-        if self.props:
+        if self.props is not None:
             for prop in self.props:
                 props_string += f" {prop}=\"{self.props[prop]}\""
         return props_string
@@ -27,7 +27,27 @@ class LeafNode(HTMLNode):
             raise ValueError("Leaf nodes must have a value")
         if not self.tag:
             return self.value
-        if self.props:
-            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, Props: {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Parent nodes must have a tag")
+        if not self.children:
+            raise ValueError("Parent nodes must have children")
+        child_string = ""
+        for child in self.children:
+            child_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{child_string}</{self.tag}>"
+    
+    def __repr__(self):
+        print(self.children)
+        return f"ParentNode({self.tag}, Children: {self.children}, Props: {self.props})"
+        
         
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -27,6 +27,62 @@ class TestLeafNode(unittest.TestCase):
     def test_to_html_none_props(self):
         node = LeafNode("p", "text", None)
         self.assertEqual("<p>text</p>", node.to_html())
+    def test_to_html_empty_props(self):
+        node = LeafNode("p", "text", {})
+        self.assertEqual("<p>text</p>", node.to_html())
     def test_to_html(self):
         node = LeafNode("a", "text", {"href": "https://www.google.com"})
         self.assertEqual('<a href="https://www.google.com">text</a>', node.to_html())
+    def test_repr(self):
+        node = LeafNode("a", "text", {"href": "https://www.google.com"})
+        self.assertEqual(
+            "LeafNode(a, text, Props: {'href': 'https://www.google.com'})",
+            repr(node)
+        )
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_none_tag(self):
+        node = ParentNode(None, [LeafNode("b", "Bold text")], None)
+        self.assertRaises(ValueError, node.to_html)
+    def test_to_html_none_children(self):
+        node = ParentNode("p", None, None)
+        self.assertRaises(ValueError, node.to_html)
+    def test_to_html_empty_children(self):
+        node = ParentNode("p", [], None)
+        self.assertRaises(ValueError, node.to_html)
+    def test_to_html_none_props(self):
+        node = ParentNode("p", [LeafNode("b", "Bold text")], None)
+        self.assertEqual("<p><b>Bold text</b></p>", node.to_html())
+    def test_to_html_child_none_tag(self):
+        node = ParentNode("p", [LeafNode(None, "text")], None)
+        self.assertEqual("<p>text</p>", node.to_html())
+    def test_to_html_child_none_value(self):
+        node = ParentNode("p", [LeafNode("b", None)], None)
+        self.assertRaises(ValueError, node.to_html)
+    def test_to_html_props(self):
+        node = ParentNode("a", [LeafNode("b", "Bold text")], {"href": "https://www.google.com"})
+        self.assertEqual('<a href="https://www.google.com"><b>Bold text</b></a>', node.to_html())
+    def test_to_html_child_props(self):
+        node = ParentNode("p", [LeafNode("a", "Link text", {"href": "https://www.google.com"})], None)
+        self.assertEqual('<p><a href="https://www.google.com">Link text</a></p>', node.to_html())
+    def test_to_html_multi_children(self):
+        node = ParentNode("p", [LeafNode("b", "Bold text"), LeafNode("i", "Italic text")], None)
+        self.assertEqual("<p><b>Bold text</b><i>Italic text</i></p>", node.to_html())
+    def test_to_html_nested_parent(self):
+        node = ParentNode("p", [ParentNode("b", [LeafNode("i", "Italic text")])], None)
+        self.assertEqual("<p><b><i>Italic text</i></b></p>", node.to_html())
+    def test_to_html_nested_parent_multi_children(self):
+        node = ParentNode("p", [ParentNode("b", [LeafNode("i", "Italic text"), LeafNode(None, "text")])], None)
+        self.assertEqual("<p><b><i>Italic text</i>text</b></p>", node.to_html())
+    def test_to_html_nested_parent_multi_branches(self):
+        node = ParentNode("p", [ParentNode("b", [LeafNode("i", "Italic text")]), LeafNode(None, "text")], None)
+        self.assertEqual("<p><b><i>Italic text</i></b>text</p>", node.to_html())
+    def test_to_html_multi_nested_parent(self):
+        node = ParentNode("p", [ParentNode("b", [LeafNode(None, "text")]), ParentNode("i", [LeafNode(None, "text")])], None)
+        self.assertEqual("<p><b>text</b><i>text</i></p>", node.to_html())
+    def test_repr(self):
+        node = ParentNode("a", [LeafNode("b", "bold"), LeafNode("i", "italic")], {"href": "https://www.google.com"})
+        self.assertEqual(
+            "ParentNode(a, Children: [LeafNode(b, bold, Props: None), LeafNode(i, italic, Props: None)], Props: {'href': 'https://www.google.com'})",
+            repr(node)
+        )
