46f22465977e15178a8a1c02d093f3f9e3d4430f
PhillGD <phillgd@outlook.com>
2024-12-12 14:58:31 +0000

    method to detect a markdown block's type

--- src/blockmarkdown.py
+++ src/blockmarkdown.py
@@ -7,3 +7,37 @@ def markdown_to_blocks(markdown):
             if markdown_block != "" and not markdown_block.isspace():
                   blocks.append(markdown_block.strip())
         return blocks
+
+def block_to_block_type(block):
+    block_type_paragraph = "paragraph"
+    block_type_heading = "heading"
+    block_type_code = "code"
+    block_type_quote = "quote"
+    block_type_ulist = "unordered_list"
+    block_type_olist = "ordered_list"
+
+    lines = block.split("\n")
+              
+    match block:
+        case block if re.match(r"^#{1,6}\s.*", block):
+            return block_type_heading
+        case block if re.match(r"^`{3}((.|\n)*)`{3}$", block):
+            return block_type_code
+        case block if re.match(r">.*", block):
+            for line in lines:
+                if not re.match(r">.*", line):
+                    return block_type_paragraph
+            return block_type_quote
+        case block if re.match(r"^(\*|-)\s.*", block):
+            for line in lines:
+                if not re.match(r"^(\*|-)\s.*", line):
+                    return block_type_paragraph
+            return block_type_ulist
+        case block if re.match(r"^1\.\s.*", block):
+            for i in range(len(lines)):
+                if not re.match(rf"^{[i + 1]}\.\s.*", lines[i]):
+                    return block_type_paragraph
+            return block_type_olist
+        case _:
+            return block_type_paragraph
+
--- src/test_blockmarkdown.py
+++ src/test_blockmarkdown.py
@@ -106,5 +106,129 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
             ],
             blocks
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_to_block_type_paragraph(self):
+        block = "This is just a line of text"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_heading(self):
+        block = "# This is a heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual("heading", block_type)
     
-        
+    def test_block_to_block_type_heading2(self):
+        block = "###### This is also a heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual("heading", block_type)
+    
+    def test_block_to_block_type_heading3(self):
+        block = "####### This is not a heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_heading4(self):
+        block = "######This is not a valid heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+    
+    def test_block_to_block_type_code(self):
+        block = "```this is code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual("code", block_type)
+
+    def test_block_to_block_type_code2(self):
+        block = "```this is code\nand so is this```"
+        block_type = block_to_block_type(block)
+        self.assertEqual("code", block_type)
+
+    def test_block_to_block_type_code3(self):
+        block = "```this is not valid code``` because of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+    
+    def test_block_to_block_type_code4(self):
+        block = "``this is not valid code```"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_quote(self):
+        block = ">This is a quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual("quote", block_type)
+
+    def test_block_to_block_type_quote2(self):
+        block = ">This is a quote\n>So is this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("quote", block_type)
+    
+    def test_block_to_block_type_quote3(self):
+        block = ">This is a quote\nBut this isn't"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_unordered(self):
+        block = "- This is an unordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual("unordered_list", block_type)
+
+    def test_block_to_block_type_unordered2(self):
+        block = "* This is an unordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual("unordered_list", block_type)
+    
+    def test_block_to_block_type_unordered3(self):
+        block = "- This is an unordered list\n- So is this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("unordered_list", block_type)
+    
+    def test_block_to_block_type_unordered4(self):
+        block = "- This is an unordered list\n* So is this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("unordered_list", block_type)
+
+    def test_block_to_block_type_unordered5(self):
+        block = "-This is an invalid unordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_unordered6(self):
+        block = "- This is an invalid unordered list\n Because of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_ordered(self):
+        block = "1. This is an ordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual("ordered_list", block_type)
+
+    def test_block_to_block_type_ordered2(self):
+        block = "1. This is an ordered list\n2. So is this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("ordered_list", block_type)
+    
+    def test_block_to_block_type_ordered3(self):
+        block = "1. This is an invalid ordered list\nBecause of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+    
+    def test_block_to_block_type_ordered4(self):
+        block = "1. This is an invalid ordered list\n1. Because of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_ordered5(self):
+        block = "1.This is an invalid ordered list"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_ordered6(self):
+        block = "1. This is an invalid ordered list\n2.Because of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
+
+    def test_block_to_block_type_ordered7(self):
+        block = "1. This is an invalid ordered list\n3. Because of this"
+        block_type = block_to_block_type(block)
+        self.assertEqual("paragraph", block_type)
