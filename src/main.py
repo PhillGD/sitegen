3b5c7589bb87de0127651c6a67694969d4772612
PhillGD <phillgd@outlook.com>
2025-04-15 01:33:29 +0100

    Change basepath logic

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ import shutil
 from copycontents import copy_files
 from sitegen import generate_page, generate_pages_recursive
 
-if len(sys.argv) > 1:
-    basepath = sys.argv[1]
-else:
-    basepath = "/"
-
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_file = "template.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Removing public directory")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
